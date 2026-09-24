92dbef08fee3f260913071a70f061814d08cd995
Matthew Yee <yee.matthew@gmail.com>
2026-09-24 10:00:21 -0700

    chore: remove validation-satellite.sh stubs for modules 02-09

--- runtime-automation/module-02-content-view/validation-satellite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# TODO: Validate - RHEL10 content view published and promoted to Library
-echo "Validating module-02-content-view" >> /tmp/progress.log
-echo "PASS: RHEL10 content view published and promoted to Library" >> /tmp/progress.log
-exit 0
--- runtime-automation/module-07-register-hosts/validation-satellite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# TODO: Validate - rhel1.lab and rhel2.lab registered in RHEL10 host group
-echo "Validating module-07-register-hosts" >> /tmp/progress.log
-echo "PASS: rhel1.lab and rhel2.lab registered in RHEL10 host group" >> /tmp/progress.log
-exit 0
